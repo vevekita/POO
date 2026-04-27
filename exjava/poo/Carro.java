d8b751cd78decc3ec311f7a69ed3c99804102df8
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-27 09:09:13 -0300

    Add cambio property and related methods in Carro

--- exjava/poo/Carro.java
+++ exjava/poo/Carro.java
@@ -5,6 +5,18 @@ public class Carro {
     private String cor;
     private String placa;
     private int numPortas;
+    private int cambio;
+    
+    public Carro(){
+
+    }
+
+    public Carro(String cor, String tipo, String placa, int numPortas){
+        this.cor = cor;
+        this.tipo = tipo;
+        this.placa = placa;
+        this.numPortas = numPortas;
+    }
     
     public void setTipo(String tipo){
         this.tipo = tipo;
@@ -34,7 +46,32 @@ public class Carro {
         this.numPortas = numPortas;
     }
 
-    public int getNumportas(){
+    public int getNumPortas(){
         return numPortas;
+    }
+    
+    public void ligar(){
+        System.out.println("CARRO LIGADO");
+    }
+    
+    public void desligar(){
+        System.out.println("DESLIGADO");
+    }
+    
+    public void acelerar(){
+        System.out.println("CARRO ACELERAnDO");
+    }
+    
+    public void frear(){
+        System.out.println("CARRO FREANDO");
+    }
+    
+    public void setCambio(int cambio){
+        this.cambio = cambio;
+    }
+    
+    public int getCambio(){
+        System.out.println("MARCHA: " + cambio);
+        return cambio;
     }
 }
