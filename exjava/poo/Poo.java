310798d81a461e93f4d657319a0dbee79a2c188f
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-27 08:13:25 -0300

    Update Poo.java

--- exjava/poo/Poo.java
+++ exjava/poo/Poo.java
@@ -2,6 +2,17 @@ package poo;
 
 public class Poo {
     public static void main(String[] args){
+        Carro meuCarro = new Carro();
+        meuCarro.setCor("PRETO");
+        meuCarro.setTipo("PASSEIO");
+        meuCarro.setPlaca("GGG-1111");
+        meuCarro.setNumPortas(4);
+
+        System.outprintln("--------Carro---------");
+        System.out.println("Cor: " + meuCarro.getCor());
+        System.out.println("Tipo: " + meuCarro.getTipo());
+        System.out.println("Placa: " + meuCarro.getPlaca());
+        System.out.println("Número de Portas: " + meuCarro.getNumPortas());
         
     }
 }
