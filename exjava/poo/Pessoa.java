440f2d7aff9e247a70767c34b9dc7f27fe7b0754
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-04-27 09:08:46 -0300

    Update Pessoa.java

--- exjava/poo/Pessoa.java
+++ exjava/poo/Pessoa.java
@@ -4,7 +4,17 @@ public class Pessoa {
     private String nome;
     private String corCabelo;
     private int idade;
+    private Carro carro;
     
+    public Pessoa(){
+        
+    }
+    
+    public Pessoa(String nome, String corCabelo, int idade){
+        this.nome = nome;
+        this.corCabelo = corCabelo;
+        this.idade = idade;
+    }
     public void setNome(String nome){
         this.nome = nome;
     }
@@ -27,5 +37,33 @@ public class Pessoa {
     
     public int getIdade(){
         return idade;
+    }
+    
+    public void setCarro(Carro carro){
+        this.carro = carro;
+    }
+    
+    public Carro getCarro(){
+        return carro;
+    }
+    
+    public void ligarCarro(){
+        carro.ligar();
+    }
+    
+    public void desligarCarro(){
+        carro.desligar();
+    }
+    
+    public void acelerarCarro(){
+        carro.acelerar();
+    }
+    
+    public void frearCarro(){
+        carro.frear();
+    }
+    
+    public void setCambioMarcha(int marcha){
+        carro.setCambio(marcha);
     }
 }
