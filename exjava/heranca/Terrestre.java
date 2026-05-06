6f608d4d204004fac2e5995598c01726fabee660
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-06 09:19:43 -0300

    Create Terrestre class extending Transporte

---
+++ exjava/heranca/Terrestre.java
@@ -0,0 +1,17 @@
+package heranca;
+
+public class Terrestre extends Transporte{
+    protected int numRodas;
+    
+    public Terrestre(){
+        
+    }
+    
+    public int getNumRodas(){
+        return numRodas;
+    }
+    
+    public void setNumRodas(int numRodas){
+        this.numRodas = numRodas;
+    }
+}
