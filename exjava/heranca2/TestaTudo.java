3d4711f2652497f7e8c6983e7e588e2413e80776
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-11 09:20:08 -0300

    Create TestaTudo.java

---
+++ exjava/heranca2/TestaTudo.java
@@ -0,0 +1,17 @@
+public class TestaTudo {
+    public static void main(String[] args) {
+        ChefeDepartamento chefe = new ChefeDepartamento();
+        Aluno aluno = new Aluno;
+        
+        chefe.setNome("Maumau");
+        chefe.setCpf("000.000.000-00");
+        chefe.setDataNasc("27/12/2007");
+        chefe.setMatricula("000000");
+        chefe.setDataAdmissao("15/10/2000");
+        chefe.setSalario();
+        chefe.setDepartamento();
+        chefe.setDataPromocao();
+        chefe.setGratificacao();
+    }
+    
+}
