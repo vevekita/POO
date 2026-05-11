de7c53cd788b269a3b10dd7563b1015b6571b1d2
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-11 09:19:14 -0300

    Create Auno.java

---
+++ exjava/heranca2/Auno.java
@@ -0,0 +1,23 @@
+public class Aluno extends Pessoa{
+    private String matricula;
+    
+    public Aluno(){
+        
+    }
+    
+    public Aluno(String nome, String cpf, String dataNasc, String matricula){
+        super(nome, cpf, dataNasc);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public void mostrarAluno(){
+        System.out.println("Aluno: " + nome);
+    }
+}
