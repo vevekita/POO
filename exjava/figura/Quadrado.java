1251422865529cbe132470659e7a9e21966de422
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-13 09:21:34 -0300

    Create Quadrado.java

---
+++ exjava/figura/Quadrado.java
@@ -0,0 +1,10 @@
+package figura;
+
+public class Quadrado extends FiguraGeometrica{
+    public Quadrado(){
+        
+    }
+    public void desenha(){
+        System.out.println("DESENHANDO QUADRADO");
+    }
+}
