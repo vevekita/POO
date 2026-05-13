1f5615106f3a06bda4e6cd231759b0574f8817f1
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-13 09:21:59 -0300

    Create Triangulo.java

---
+++ exjava/figura/Triangulo.java
@@ -0,0 +1,10 @@
+package figura;
+
+public class Triangulo extends FiguraGeometrica{
+    public Triangulo(){
+        
+    }
+    public void desenha(){
+        System.out.println("DESENHANDO TRIÂNGULO");
+    }
+}
