97da8663e968381cad9f95fc57076d8873c1bdd4
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-13 09:20:45 -0300

    Create FiguraGeometrica.java

---
+++ exjava/figura/FiguraGeometrica.java
@@ -0,0 +1,10 @@
+package figura;
+
+public class FiguraGeometrica {
+    public FiguraGeometrica(){
+        
+    }
+    public void desenha(){
+        System.out.println("DESENHANDO FIGURA");
+    }
+}
