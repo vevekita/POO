cb0f7ef55ecc3c89c4a7de6cb3bf26e368979a33
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-18 09:24:21 -0300

    Create empada.java

---
+++ exjava/Receita/empada.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comida;
+
+public class Empada extends Receita implements Assado{
+    private String recheio;
+    private int quantidade;
+    
+    public Empada(String tipoReceita){
+        super("Salgado");
+    }
+    
+    @Override
+    public String getIngredientes(){
+        return "jakjldkjaf";
+    }
+    
+    @Override
+    
+    
+}
