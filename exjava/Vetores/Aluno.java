e9f181283220374e225cd69c735ce913c3064b81
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-05-20 09:06:17 -0300

    Create Aluno.java

---
+++ exjava/Vetores/Aluno.java
@@ -0,0 +1,16 @@
+package aula;
+
+public class Aluno {
+    private String nome;
+    
+    public Aluno(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
