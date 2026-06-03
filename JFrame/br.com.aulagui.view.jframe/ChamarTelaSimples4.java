1f3a1cddc34629878f7821bee3675d1267e31229
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-03 08:41:47 -0300

    Add ChamarTelaSimples4 class to launch TelaSimples4

---
+++ JFrame/br.com.aulagui.view.jframe/ChamarTelaSimples4.java
@@ -0,0 +1,7 @@
+package br.com.aulagui.view.jframe;
+
+public class ChamarTelaSimples4 {
+    public static void main(String[] args){
+        TelaSimples4 ts4 = new TelaSimples4("Tela Simples 4");
+    }
+}
