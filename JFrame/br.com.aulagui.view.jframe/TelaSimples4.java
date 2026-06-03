0369d999aada09dfc191eacad9b74e91c469fc3d
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-03 08:41:08 -0300

    Create TelaSimples4.java

---
+++ JFrame/br.com.aulagui.view.jframe/TelaSimples4.java
@@ -0,0 +1,15 @@
+/*outra forma de instanciar uma tela*/
+package br.com.aulagui.view.jframe;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class TelaSimples4 extends JFrame{
+    TelaSimples4(String titulo){
+        super(titulo);
+        setSize(640, 480);
+        getContentPane().setBackground(Color.red);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
