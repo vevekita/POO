bd744689829999b849659223e3e5260badf0060d
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-03 08:58:00 -0300

    Delete JFrame/br.com.aulagui.view.jframe directory

--- JFrame/br.com.aulagui.view.jframe/ChamarTelaSimples4.java
+++
@@ -1,7 +0,0 @@
-package br.com.aulagui.view.jframe;
-
-public class ChamarTelaSimples4 {
-    public static void main(String[] args){
-        TelaSimples4 ts4 = new TelaSimples4("Tela Simples 4");
-    }
-}
