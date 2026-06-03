a16f524344e2f35fc7ef0c40a01c1f749128c202
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-03 09:02:37 -0300

    Add InputDialogGUI for summing two numbers

---
+++ JFrame/br.com.aulagui.view.joptionpane/InputDialogGUI.java
@@ -0,0 +1,18 @@
+package br.com.aulagui.view.joptionpane;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+public class InputDialogGUI {
+    public static void main(String[] args){
+        String s1 = JOptionPane.showInputDialog(null, "Primeiro Número", "Entrada", JOptionPane.QUESTION_MESSAGE);
+        
+        String s2 = JOptionPane.showInputDialog(null, "Segundo Número", "Entrada", JOptionPane.QUESTION_MESSAGE);
+        
+        int n1 = Integer.parseInt(s1);
+        int n2 = Integer.parseInt(s2);
+        int sum = n1 + n2;
+        
+        JOptionPane.showMessageDialog(null, "A soma é: " + sum, "Soma de Dois números", JOptionPane.PLAIN_MESSAGE);
+    }
+}
