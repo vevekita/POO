00fe3f7f8e21a26cfac78e6335068dc746548dfa
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-24 09:05:12 -0300

    Create Estado.java

---
+++ exercicio/Estado.java
@@ -0,0 +1,65 @@
+package pais;
+
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author guest-2k9yhc
+ */
+
+@Entity
+@Table(name = "ESTADO")
+public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO);
+    private long id; 
+    private String unidadeFederativa;
+    private String nome;
+    private List<Cidade> cidades;
+    private Governador governador;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUnidadeFederativa() {
+        return unidadeFederativa;
+    }
+
+    public void setUnidadeFederativa(String unidadeFederativa) {
+        this.unidadeFederativa = unidadeFederativa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+
+    public Governador getGovernador() {
+        return governador;
+    }
+
+    public void setGovernador(Governador governador) {
+        this.governador = governador;
+    }
+}
