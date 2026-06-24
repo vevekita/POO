5a93037731f5bc875b3ff7092a1574a9278d847a
Verônica Kitamura <108936525+vevekita@users.noreply.github.com>
2026-06-24 09:10:11 -0300

    Create Governador.java

---
+++ exercicio/Governador.java
@@ -0,0 +1,33 @@
+package pais;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+/**
+ *
+ * @author guest-2k9yhc
+ */
+@Entity
+public class Governador {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
